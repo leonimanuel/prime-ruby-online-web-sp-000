414193e7759cb7d13958d5ab87f9277be2781afe
Leon Malisov <leonmalisov@gmail.com>
2020-01-11 23:23:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require "pry"
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  i = 2
+  divisors = []
+  while i <= (num / 2)
+    divisors << i
+    i += 1
+  end
+  divisors.all? {|x| num % x != 0}
+end
+
+# binding.pry
